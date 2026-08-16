55b1e50d6ea0f5ebd85ccae71b621343a2efe784
matchalatte1200 <root@LAPTOP-FFIAREM5.localdomain>
2026-08-17 07:44:31 +0900

    Add NTP server fallback

--- src/main.rs
+++ src/main.rs
@@ -6,23 +6,19 @@ mod metrics;
 
 fn main() {
     env_logger::Builder::from_env(Env::default().default_filter_or("info"))
-    .format_timestamp(None)
-    .init();
+        .format_timestamp(None)
+        .init();
     let mut sys = System::new_all();
     let mut disks = Disks::new_with_refreshed_list();
 
-    let ntp_servers = [
-    "ntp.nict.jp:123",
-    "time.nist.gov:123",
-    "pool.ntp.org:123",
-    ];
+    let ntp_servers = ["ntp.nict.jp:123", "time.nist.gov:123", "pool.ntp.org:123"];
 
     let ntp_time = match metrics::time::NtpClock::new(&ntp_servers) {
-    Some(ntp_time) => ntp_time,
-    None => {
-        info!("Failed to synchronize with all NTP servers");
-        return;
-      }
+        Some(ntp_time) => ntp_time,
+        None => {
+            info!("Failed to synchronize with all NTP servers");
+            return;
+        }
     };
 
     loop {
@@ -31,12 +27,12 @@ fn main() {
         let disk_usage = metrics::disk::get_disk_usage(&mut disks);
 
         info!(
-        "[{}] CPU: {:.2}%, Memory available: {:.2}%, Disk: {:.2}%",
-        ntp_time.now().format("%Y-%m-%d %H:%M:%S%.3f"),
-        cpu_usage,
-        available_memory_percentage,
-        disk_usage
-    );
+            "[{}] CPU: {:.2}%, Memory available: {:.2}%, Disk: {:.2}%",
+            ntp_time.now().format("%Y-%m-%d %H:%M:%S%.3f"),
+            cpu_usage,
+            available_memory_percentage,
+            disk_usage
+        );
         std::thread::sleep(std::time::Duration::from_secs(5));
     }
 }
--- src/metrics/time.rs
+++ src/metrics/time.rs
@@ -1,7 +1,7 @@
 use chrono::{DateTime, Duration, Local, TimeZone};
+use log::warn;
 use rsntp::SntpClient;
 use std::time::Instant;
-use log::warn;
 
 pub struct NtpClock {
     start_time: DateTime<Local>,
@@ -9,37 +9,44 @@ pub struct NtpClock {
 }
 
 impl NtpClock {
-    pub fn new(ntp_servers:&[&str]) -> Option<Self> {
+    pub fn new(ntp_servers: &[&str]) -> Option<Self> {
         let client = SntpClient::new();
 
-        for server in ntp_servers{
-            match client.synchronize(server){
-                Ok(response)=>{
-                let duration = response.datetime().unix_timestamp().ok()?;
+        for server in ntp_servers {
+            match client.synchronize(server) {
+                Ok(response) => {
+                    let duration = response.datetime().unix_timestamp().ok()?;
 
-                let start_time = Local
-                    .timestamp_opt(duration.as_secs() as i64, duration.subsec_nanos())
-                    .single()?;
+                    let start_time = Local
+                        .timestamp_opt(
+                            duration.as_secs() as i64,
+                            duration.subsec_nanos(),
+                        )
+                        .single()?;
 
-                return Some(Self {
-                    start_time,
-                    start_instant: Instant::now(),
-                });        
-            }
-            Err(error) => {
-            warn!("Failed to synchronize with NTP server {}: {}", server, error);
+                    return Some(Self {
+                        start_time,
+                        start_instant: Instant::now(),
+                    });
+                }
+                Err(error) => {
+                    warn!(
+                        "Failed to synchronize with NTP server {}: {}",
+                        server, error
+                    );
+                }
             }
         }
-    warn!("Failed to synchronize with all NTP servers");
-     None
-  }
 
-pub fn now(&self) -> DateTime<Local> {
+        warn!("Failed to synchronize with all NTP servers");
+        None
+    }
+
+    pub fn now(&self) -> DateTime<Local> {
         let elapsed = self.start_instant.elapsed();
 
         self.start_time
             + Duration::from_std(elapsed)
                 .expect("elapsed duration should be valid")
     }
-  }
 }
