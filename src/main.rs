15cd9a5065eaa211699996742cff4f5feca0701c
matcha <tomo20030217@gmail.com>
2026-08-16 21:39:13 +0900

    Add rustfmt configuration

--- src/main.rs
+++ src/main.rs
@@ -2,37 +2,39 @@ use std::path::Path;
 
 use env_logger::Env;
 use log::error;
-use sysinfo::{Disks, MINIMUM_CPU_UPDATE_INTERVAL, System};
 use log::info;
 use log::warn;
+use sysinfo::{Disks, MINIMUM_CPU_UPDATE_INTERVAL, System};
 fn main() {
     env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
     let mut sys = System::new_all();
     let mut disks = Disks::new_with_refreshed_list();
     sys.refresh_cpu_usage();
-    loop{
-    std::thread::sleep(MINIMUM_CPU_UPDATE_INTERVAL);
-    sys.refresh_cpu_usage();
-    sys.refresh_memory();
-    disks.refresh(true);
+    loop {
+        std::thread::sleep(MINIMUM_CPU_UPDATE_INTERVAL);
+        sys.refresh_cpu_usage();
+        sys.refresh_memory();
+        disks.refresh(true);
 
-    let cpu_usage = sys.global_cpu_usage();
-    let total_memory = sys.total_memory() as f64;
-    let available_memory = sys.available_memory() as f64;
-    let available_memory_percentage = (available_memory / total_memory) * 100.0;
+        let cpu_usage = sys.global_cpu_usage();
+        let total_memory = sys.total_memory() as f64;
+        let available_memory = sys.available_memory() as f64;
+        let available_memory_percentage = (available_memory / total_memory) * 100.0;
 
-    let mut disk_usage =  0.0;
+        let mut disk_usage = 0.0;
 
-    for disk in disks.list(){
-        if disk.mount_point() == Path::new("/") {
-            let total = disk.total_space() as f64 ;
-            let available = disk.available_space() as f64;
-            disk_usage = (total - available) / total * 100.0;
+        for disk in disks.list() {
+            if disk.mount_point() == Path::new("/") {
+                let total = disk.total_space() as f64;
+                let available = disk.available_space() as f64;
+                disk_usage = (total - available) / total * 100.0;
+            }
         }
-    }
-    
-    info!("CPU: {:.2}%, Memory available: {:.2}%, Disk: {:.2}%", cpu_usage, available_memory_percentage, disk_usage);
-    std::thread::sleep(std::time::Duration::from_secs(5));
-    }
 
+        info!(
+            "CPU: {:.2}%, Memory available: {:.2}%, Disk: {:.2}%",
+            cpu_usage, available_memory_percentage, disk_usage
+        );
+        std::thread::sleep(std::time::Duration::from_secs(5));
+    }
 }
