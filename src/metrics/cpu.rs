cdd88154dfd4e868e131572688685360aa321c08
matchalatte1200 <tomo20030217@gmail.com>
2026-08-18 08:33:35 +0900

    chang to f64

--- src/metrics/cpu.rs
+++ src/metrics/cpu.rs
@@ -1,6 +1,6 @@
 use sysinfo::{MINIMUM_CPU_UPDATE_INTERVAL, System};
 
-pub fn get_cpu_usage(sys: &mut System) -> f32 {
+pub fn get_cpu_usage(sys: &mut System) -> f64 {
     sys.refresh_cpu_usage();
     std::thread::sleep(MINIMUM_CPU_UPDATE_INTERVAL);
     sys.refresh_cpu_usage();
