2a8e1745d519c0bdb86230c29b6b3fa2184f17b2
matchalatte1200 <tomo20030217@gmail.com>
2026-08-18 08:37:14 +0900

    change to sys.global_cpu_usage().into() in cpu.rs

--- src/metrics/cpu.rs
+++ src/metrics/cpu.rs
@@ -4,5 +4,5 @@ pub fn get_cpu_usage(sys: &mut System) -> f64 {
     sys.refresh_cpu_usage();
     std::thread::sleep(MINIMUM_CPU_UPDATE_INTERVAL);
     sys.refresh_cpu_usage();
-    sys.global_cpu_usage()
+    sys.global_cpu_usage().into()
 }
